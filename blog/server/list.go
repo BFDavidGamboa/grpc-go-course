e2414abee43de85693cefe9a0fd2784ff5d28c09
BFDavidGamboa <david.gamboa@blankfactor.com>
2022-12-28 12:18:47 -0500

    Added commit from dev branch which includes delete, update, list endpoints

---
+++ blog/server/list.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/BFDavidGamboa/grpc-go-course/blog/proto"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (s *Server) ListBlogs(in *emptypb.Empty, stream pb.BlogService_ListBlogsServer) error {
+	log.Println("ListBlogs was invoked")
+	cur, err := collection.Find(context.Background(), primitive.D{})
+
+	if err != nil {
+		return status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Unkown internal error: %v\n", err),
+		)
+	}
+
+	defer cur.Close(context.Background())
+
+	for cur.Next(context.Background()) {
+		data := &BlogItem{}
+		err := cur.Decode(data)
+
+		if err != nil {
+			return status.Errorf(codes.Internal,
+				fmt.Sprintf("Error while decoding data from mongodb: %v\n", err),
+			)
+		}
+		stream.Send(documentToBlog(data))
+	}
+	if err = cur.Err(); err != nil {
+		return status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Unkown internal error: %v\n", err),
+		)
+	}
+
+	return nil
+}
